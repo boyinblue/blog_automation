96d1c2792e13c80017a46a66c8fddf55de4331c5
boyinblue <boyinblue@naver.com>
2022-03-06 01:53:29 +0900

    자동화 분석 누락 페이지 수정

--- check_target_link.sh
+++ check_target_link.sh
@@ -1,44 +1,119 @@
 #!/bin/bash
 
-BLOG_URL="https://frankler.tistory.com"
+declare -A arrData
+declare -A arrLinkWarning
 
-function check_page()
+BLOG_URL="https://frankler.tistory.com"
+if [ "${1}" != "" ]; then
+  BLOG_URL="${1}"
+fi
+
+function check_link()
 {
-  echo "check_page ${1}"
+#  echo "check_link(${filename}, ${line})"
+
+  if [[ "${line}" == *"target="* ]]; then
+    arrLinkWarning[${filename}]+="${line}<br>\n"
+#    echo "[$filename] ${line}"
+  fi
+}
+
+function print_link_warning()
+{
+  echo "${arrLinkWarning[@]}"
+}
+
+function get_title()
+{
+#  echo "get_title(${filename}, ${line})"
+
+  if [[ "${line}" == *"<title>"* ]]; then
+    title=${line##<title>}
+    title2=${title%%</title>}
+    arrKey+="${filename} "
+    arrData[${filename}_title]="${title2}"
+    arrData[${filename}_url]="${BLOG_URL}/${filename}"
+  fi
+}
+
+function print_title_info()
+{
+  echo "<table>"
+  echo "  <tr>"
+  echo "    <td>No.</td>"
+  echo "    <td>URL</td>"
+  echo "    <td>Title</td>"
+  echo "  </tr>"
+  for local_filename in ${arrKey[@]}
+  do
+    echo "  <tr>"
+    echo "    <td>${local_filename}</td>"
+    echo "    <td>${arrData[${local_filename}_url]}</td>"
+    echo "    <td>${arrData[${local_filename}_title]}</td>"
+    echo "  </tr>"
+  done
+  echo "</table>"
+}
+
+function check_next()
+{
+#  echo "check_next(${line})"
+
+  if [[ "$line" == *"<a href=\"/"* ]]; then
+    line2=${line##<a href=\"/}
+#    echo "$line"
+#    echo "$line2"
+    line3=${line2%%\">*}
+    number="${line3%%\?*}"
+    if [[ "$number" =~ ^[0-9]+$ ]]; then
+      download_page "$number"
+    elif [ "${number}" != "" ] && [ "${number:0:3}" != "tag" ]; then
+      echo "No number : ${number} ${line}"
+    fi
+  #<a  href='?page=2'>
+  elif [[ "$line" == *"<a  href='?page="* ]]; then
+    echo "New type : ${line}"
+    line2=${line##*<a  href=\'}
+    line3=${line2%%\'*}
+    echo "$line2"
+    echo "$line3"
+    if [ "${line3:0:6}" == "?page=" ]; then
+      download_page "${line3}"
+    fi
+  fi
+}
+
+function download_page()
+{
+#  echo "check_page ${1}"
   if [ "${1}" == "" ]; then
     filename="index.html"
-    echo "wget ${BLOG_URL}"
-    wget -O "${filename}" "${BLOG_URL}" >> /dev/null
+    echo "wget -q -O ${filename} ${BLOG_URL}"
+    wget -q -O "${filename}" "${BLOG_URL}" >> /dev/null
   elif [ -e ${1} ]; then
     return
   else
-    echo "wget ${BLOG_URL}/${1}"
-    wget -O "${1}" "${BLOG_URL}/${1}" >> /dev/null
+    echo "wget -q -O ${1} ${BLOG_URL}/${1}"
+    wget -q -O "${1}" "${BLOG_URL}/${1}" >> /dev/null
     filename="${1}"
   fi
 
   echo "Check file : ${filename}"
   while read line  
   do
-    if [[ "$line" == *"target="* ]]; then
-      echo "[$filename] $line"
+    if [[ "$filename" =~ ^[0-9]+$ ]]; then
+    check_link ${filename} ${line}
+    get_title ${filename} ${line}
     fi
-    if [[ "$line" == *"<a href=\"/"* ]]; then
-      line2=${line##<a href=\"/}
-#      echo "$line"
-#      echo "$line2"
-      line3=${line2%%\">*}
-      number="${line3%%\?*}"
-      if [[ "$number" =~ ^[0-9]+$ ]]; then
-        echo "$number"
-        check_page "$number"
-      fi
-    fi
+    check_next ${line}
   done < ${filename}
 }
 
 rm -rf tmp
 mkdir -p tmp
 pushd tmp
-check_page
+download_page
 popd
+
+print_title_info
+print_link_warning
