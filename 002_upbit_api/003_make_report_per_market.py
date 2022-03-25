3b8b0f468dedd98df7af0edb2c493e668e8fb4ab
boyinblue <boyinblue@naver.com>
2022-03-25 14:42:05 +0900

    additional change

--- 002_upbit_api/003_make_report_per_market.py
+++ 002_upbit_api/003_make_report_per_market.py
@@ -32,9 +32,6 @@ if date == "":
     print("Please set date to analysis")
     exit()
 
-def select_data(data, field):
-    return data[field]
-
 def draw_graph():
     plt.plot(days, trade_price)
 
@@ -65,38 +62,52 @@ def write_table(date, filename, title, description, json_data):
                 candle_json_data['high_price'] / 1000,
                 candle_json_data['trade_price'] / 1000 ) )
 
-def load_data_from_upbit(date, market_name, filename, title, description):
+def write_readme(date, json_data):
+    readme_filename = "{}/{}/README.md".format(TARGET_DIR, date)
+    print("Writing File : {}".format(readme_filename))
+    f_readme = open(readme_filename, 'w')
+    f_readme.write("---\n")
+    f_readme.write("title: 가상화폐 일간 통계 ({})\n".format(date))
+    f_readme.write("description: 가상화폐 일간 통계 ({})\n".format(date))
+    f_readme.write("---\n")
+    f_readme.write("\n")
 
-    with open("tmp/{}/daily_candle_{}.json".format(date, market_name), 'r') as json_file:
-        json_data = json.load(json_file)
-        write_table(date, filename, title, description, json_data)
+    f_readme.write("|종목|종목 코드|종가|거래량|전일 대비|변화량|링크|\n")
+    f_readme.write("|--|--|--|--|--|--|--|\n")
+    for single_data in json_data:
+        f_readme.write("|{}|{} ({})|{}|{}|{}|{}|[{}]({})\n".format(
+            single_data['korean_name'],
+            single_data['market'],
+            single_data['english_name'],
+            single_data['detail'][0]['trade_price'],
+            single_data['detail'][0]['candle_acc_trade_price'],
+            single_data['detail'][0]['change_price'],
+            single_data['detail'][0]['change_rate'],
+            single_data['title'],
+            single_data['filename']) )
 
 #if False == os.path.isfile( TARGET_DIR ):
 if False == os.path.islink( TARGET_DIR ):
     print("There is no target directory")
     exit()
 
-readme_filename = "{}/{}/README.md".format(TARGET_DIR, date)
-print("Writing File : {}".format(readme_filename))
-f_readme = open(readme_filename, 'w')
-f_readme.write("---\n")
-f_readme.write("title: 가상화폐 일간 통계 ({})\n".format(date))
-f_readme.write("description: 가상화폐 일간 통계 ({})\n".format(date))
-f_readme.write("---\n")
-f_readme.write("\n")
-
 with open("tmp/market_code.json") as json_file:
     json_data = json.load(json_file)
     for market_json_data in json_data:
-        market_name = market_json_data['market']
+        market_name = market_json_data['korean_name']
+        market_code = market_json_data['market']
         print( "market : ", market_name )
         title = "가상화폐 일간 통계 ({}, {})".format( market_name, date)
         description = title
 #        filename = "/" + date[:10] + "-daily-candle-10days"
-        filename = "{}/{}/{}-daily-candle-10days".format( TARGET_DIR, date[:10],market_name )
+        filename = "{}/{}/{}-daily-candle-10days".format( TARGET_DIR, date[:10],market_code )
 
-        load_data_from_upbit(date, market_json_data['market'], filename, title, description)
-        
-        filename = "{}-daily-candle-10days".format(market_name )
-        f_readme.write("[{}]({}.html)<br>\n".format(title, filename))
-        f_readme.write("[{}]({}.md)<br>\n".format(title, filename))
+        market_json_data['title'] = title
+        market_json_data['description'] = description
+        market_json_data['filename'] = filename
+
+        with open("tmp/{}/daily_candle_{}.json".format(date, market_json_data['market']), 'r') as fp:
+            market_json_data['detail'] = json.load(fp)
+            write_table(date, filename, title, description, market_json_data['detail'])
+
+    write_readme(date[:10], json_data)
