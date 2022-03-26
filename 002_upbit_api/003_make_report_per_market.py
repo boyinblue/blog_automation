6862e8db13cd8af139c0aef8330a49a48500a048
boyinblue <boyinblue@naver.com>
2022-03-26 17:38:18 +0900

    업비트 자동화 디테일 화면 꾸미기

--- 002_upbit_api/003_make_report_per_market.py
+++ 002_upbit_api/003_make_report_per_market.py
@@ -32,6 +32,10 @@ if date == "":
     print("Please set date to analysis")
     exit()
 
+def change_date_form(str_date):
+    date = datetime.datetime.strptime(str_date, "%Y-%m-%dT%H:%M:%S")
+    return datetime.datetime.strftime(date, "%Y년 %m월 %d일 %H시")
+
 def draw_graph():
     plt.plot(days, trade_price)
 
@@ -55,15 +59,25 @@ def write_table(filepath, json_data, detail_json_data):
         f.write("|--|--|\n")
         f.write("|종목|{}|\n".format(json_data['korean_name']))
         f.write("|마켓|{}|\n".format(json_data['market']))
+
+        scale = json_data['market'].split()[0]
+        f.write("|단위|{}|\n".format(scale))
+
         days = json_data['days']
-        f.write("|종류|일 단위 캔들 ({}일간)|\n".format(days))
-        date_from = detail_json_data[days-1]['candle_date_time_kst'][:13]
-        date_from.replace("T", " ")
-        date_to = detail_json_data[0]['candle_date_time_kst'][:13]
-        date_to.replace("T", " ")
-        f.write("|기간|{} - {}\n|".format(date_from, date_to))
+        f.write("|종류|일 단위 캔들 ({}일치)|\n".format(days))
+
+        str_date = detail_json_data[days-1]['candle_date_time_kst']
+        str_date_from = change_date_form(str_date)
+        str_date = detail_json_data[0]['candle_date_time_kst']
+        str_date_to = change_date_form(str_date)
+        f.write("|기간|{} - {}|\n".format(str_date_from, str_date_to))
+
+        date = datetime.datetime.today().strftime("%Y년 %m월 %d일 %H시")
+        f.write("|생성일|{}|\n".format(date))
+        f.write("|출처|[업비트 Open API](https://docs.upbit.com)|\n")
         f.write("\n")
         f.write("\n")
+
         f.write("|날짜|시가|저가|고가|종가|비고|\n")
         f.write("|--|--|--|--|--|--|\n")
         for candle_json_data in detail_json_data:
@@ -73,6 +87,30 @@ def write_table(filepath, json_data, detail_json_data):
                 candle_json_data['low_price'],
                 candle_json_data['high_price'],
                 candle_json_data['trade_price'] ) )
+
+        f.write("\n")
+        f.write("\n")
+        f.write("일간 캔들 구간\n")
+        f.write("---\n")
+        f.write("업비트의 일간 캔들은 세계 협정시(UTC) 기준입니다. \n")
+        f.write("UTC 기준 0시 0분 0초부터 23시 59분 59초까지입니다. \n")
+        f.write("한국 시간인 KST 기준으로는 전일 오전 9시부터 다음날 오전 8시 59분까지입니다. \n")
+        f.write("\n")
+        f.write("\n")
+
+        f.write("예를들면, UTC 기준 3월 25일 데이터는 UTC 시준 3월 25일 0시 0분 0초부터 3월 25일 23시 59분 59초까지입니다. \n")
+        f.write("한국 시간으로 표현하면 3월 25일 오전 9시부터 3월 26일 오전 8시 59분까지입니다. \n")
+        f.write("이와 같은 이유로 본 페이지는 매일 오전 9시마다 전날의 통계 데이터를 자동으로 작성합니다. \n")
+        f.write("\n")
+        f.write("\n")
+
+        f.write("주의\n")
+        f.write("---\n")
+        f.write("\n")
+        f.write("\n")
+        f.write("본 페이지는 업비트의 Open API를 통하여 자동으로 생성된 페이지입니다. \n")
+        f.write("단순한 통계 데이터만 제공하고, 자료를 통해 인사이트를 얻기 위한 참고용으로만 사용하시기 바랍니다. \n")
+        f.write("투자를 권유하는 것은 아니며, 투자에 대한 모든 책임은 투자자 본인에게 있습니다. \n")
 
 def write_readme(date, json_data):
     readme_filename = "{}/{}/README.md".format(TARGET_DIR, date)
@@ -128,7 +166,10 @@ with open("tmp/market_code.json", 'r') as json_file:
         market_name = market_json_data['korean_name']
         market_code = market_json_data['market']
         print( "market : {} ({})".format(market_code, market_name) )
-        title = "가상화폐 일간 통계 ({}, {})".format( market_name, date)
+        title = "가상화폐 일간 통계 ({}, {}, {})".format(
+                        market_name, 
+                        date,
+                        market_code)
         description = title
 #       filename = "/" + date[:10] + "-daily-candle-10days"
         filename = "{}-daily-candle-10days.html".format(market_code)
