db67b2614713360d5e373b97e5a6688f47ab1399
parksejin <parksejin@parksejin-desktop>
2022-05-17 13:51:32 +0900

    update

--- 004_epass/send_article.py
+++ 004_epass/send_article.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods import posts
 
+def write_post(url, id, pw, title, slug, content):
+  print("id :", id)
+  print("pw :", pw)
+  url = url + "/xmlrpc.php"
+  print("url :", url)
+  client = Client(url, id, pw)
+  post = WordPressPost()
+  post.title = title
+  post.slug = slug
+  post.content = content
+  post.terms_names = {
+                  'post_tag': 'wordpress',
+                  'category' : ['']
+  }
 
-print("abc")
+  post.post_status = 'publish'
+  client.call(posts.NewPost(post))
+
+if __name__ == '__main__':
+  write_post( "https://www.dhqhrtnwl.shop",
+                  'esregnet0409@gmail.com',
+                  'honor0904',
+                  "자동 글쓰기",
+                  "자동 글쓰기 테스트",
+                  "자동 글쓰기 본문")
