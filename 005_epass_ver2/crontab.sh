69cff8748eabf51cbea0919cfb8f88a66ab4675e
boyinblue <boyinblue@naver.com>
2022-11-18 14:37:20 +0900

    update

--- 005_epass_ver2/crontab.sh
+++ 005_epass_ver2/crontab.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 git pull
+pushd ../../boyinblue.github.io
+git pull
+popd
+
 ./accumulate.py
 ./process.py
+
+pushd ../../boyinblue.github.io
+git add .
+git commit -m "[Automatic Commit] Event Info Generate"
+git push
+popd
--- 005_epass_ver2/process.py
+++ 005_epass_ver2/process.py
@@ -52,8 +52,8 @@ def write_info_by_exp_date(dic):
     if not os.path.exists(info_fname_by_exp_dat):
         fp = open(info_fname_by_exp_dat, "w")
         fp.write("---\n")
-        fp.write("title: {} 종료되는 이벤트 정보".format(exp_date_str))
-        fp.write("description: {}에 종료되는 이벤트 정보들을 제공합니다.".format(exp_date_str))
+        fp.write("title: {} 종료되는 이벤트 정보\n".format(exp_date_str))
+        fp.write("description: {}에 종료되는 이벤트 정보들을 제공합니다.\n".format(exp_date_str))
         fp.write("---\n")
         fp.write(dic['md'])
         return
